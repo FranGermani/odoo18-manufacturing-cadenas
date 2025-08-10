113f814d6003cc45605486841a20e70fa19611bf
franciscoGermani <franciscogermani98@gmail.com>
2025-08-10 12:43:47 -0300

    primer repo

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Manufacturing Custom Module',
+    'version': '1.0',
+    'category': 'Manufacturing',
+    'summary': 'caso de uso de produccion para cadenas metalicas',
+    'description': """
+        Ejercicio de configuracion del modulo manufacturing para la gestion de cadenas metalicas.
+    """,
+    'author': 'FranG',
+    'depends': ['mrp', 'stock'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
